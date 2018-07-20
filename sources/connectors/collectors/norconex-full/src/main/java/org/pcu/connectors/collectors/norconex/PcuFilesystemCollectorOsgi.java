bc3c14fcd2639e21d7c834c5953fe819cd8d3025
Gaëlle FOURNIER <gaelle.fournier.work@gmail.com>
2018-07-20 17:05:30 +0200

    Working norconex-full, but without variables

--- sources/connectors/collectors/norconex-full/src/main/java/org/pcu/connectors/collectors/norconex/PcuFilesystemCollectorOsgi.java
+++ sources/connectors/collectors/norconex-full/src/main/java/org/pcu/connectors/collectors/norconex/PcuFilesystemCollectorOsgi.java
@@ -1,22 +1,34 @@
 package org.pcu.connectors.collectors.norconex;
 
-import javax.xml.validation.SchemaFactory;
+import java.util.Dictionary;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-
-import com.norconex.commons.lang.config.XMLConfigurationUtil;
 
 public class PcuFilesystemCollectorOsgi implements BundleActivator {
 
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		System.out.println("Start.");
+		//Dictionary<String, String> dict = bundleContext.getBundle().getHeaders();
+//		dict.put("javax.xml.validation.SchemaFactory:http://www.w3.org/XML/XMLSchema/v1.0",
+//				"org.apache.xerces.jaxp.validation.XMLSchemaFactory");
+//		dict.put("javax.xml.validation.SchemaFactory:http://www.w3.org/XML/XMLSchema/v1.1",
+//				"org.apache.xerces.jaxp.validation.XMLSchema11Factory");
+//		System.setProperty("jaxp.debug", "true");
+		//SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1");
+		//SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1");
+				
 		
-		System.setProperty("jaxp.debug", "true");
-		SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.0");
-		PcuFilesystemCollector pcuFilesystemCollector = new PcuFilesystemCollector(bundleContext);
-		pcuFilesystemCollector.execute();
+		Thread thread = Thread.currentThread();
+		ClassLoader loader = thread.getContextClassLoader();
+		thread.setContextClassLoader(this.getClass().getClassLoader());
+		try {
+			PcuFilesystemCollector pcuFilesystemCollector = new PcuFilesystemCollector(bundleContext);
+			pcuFilesystemCollector.execute();
+		} finally {
+		  thread.setContextClassLoader(loader);
+		}
 	}
 
 	@Override
