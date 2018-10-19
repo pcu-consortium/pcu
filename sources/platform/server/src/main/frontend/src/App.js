f6cd2ff22da3be9b51c15adefae2b9966a4f3777
Gaëlle FOURNIER <gaelle.fournier@smile.fr>
2018-10-19 17:53:57 +0200

    React front deploy with spring-boot + use create-react-app

---
+++ sources/platform/server/src/main/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style/main.css'
+
+import Search from './scenes/Search/Search.js';
+
+class App extends Component {
+  
+  render() {
+    return (
+        <Search />
+    );
+  }
+}
+
+export default App;
